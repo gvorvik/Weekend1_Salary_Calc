209eb7f3b9e722607f3e2dbd21d62846b9aa7201
Greg Orvik <gvorvik@gmail.com>
2018-04-13 17:12:59 -0500

    Append Employee to DOM on Button Click

--- script.js
+++ script.js
@@ -4,4 +4,26 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQuery Sourced');
+    $('#submitButton').on('click', submitButtonClick);
+}
+
+function submitButtonClick() {
+    let first = $('#firstNameInput');
+    let last = $('#lastNameInput');
+    let id = $('#idInput');
+    let title = $('#titleInput');
+    let salary = $('#salaryInput');
+    
+    if(first.val() === '' || last.val() === '' || id.val() === '' 
+    || title.val() === '' || salary.val() === '') {
+       return alert('You must complete all the fields!');
+    }
+
+    $('tbody').append(`<tr>
+        <td>${first.val()}</td>
+        <td>${last.val()}</td>
+        <td>${id.val()}</td>
+        <td>${title.val()}</td>
+        <td>${salary.val()}</td>
+        </tr>`);   
 }
