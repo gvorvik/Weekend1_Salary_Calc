1bc3608ad29617640becff7517285d42679f60be
Greg Orvik <gvorvik@gmail.com>
2018-04-14 18:14:08 -0500

    Append the cost to the DOM

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log('ScriptJS Sourced');
+//Set Monthly Cost
+let cost = 0;
 
 $(document).ready(onReady);
 
@@ -8,22 +10,43 @@ function onReady() {
 }
 
 function submitButtonClick() {
-    let first = $('#firstNameInput');
-    let last = $('#lastNameInput');
-    let id = $('#idInput');
-    let title = $('#titleInput');
-    let salary = $('#salaryInput');
-    
-    if(first.val() === '' || last.val() === '' || id.val() === '' 
-    || title.val() === '' || salary.val() === '') {
+    //Set variables for inputs
+    let $first = $('#firstNameInput');
+    let $last = $('#lastNameInput');
+    let $id = $('#idInput');
+    let $title = $('#titleInput');
+    let $salary = $('#salaryInput');
+
+    //Make sure every input is full
+    if($first.val() === '' || $last.val() === '' || $id.val() === '' 
+    || $title.val() === '' || $salary.val() === '') {
        return alert('You must complete all the fields!');
     }
 
+    //Append the values to the DOM
     $('tbody').append(`<tr>
-        <td>${first.val()}</td>
-        <td>${last.val()}</td>
-        <td>${id.val()}</td>
-        <td>${title.val()}</td>
-        <td>${salary.val()}</td>
-        </tr>`);   
+        <td>${$first.val()}</td>
+        <td>${$last.val()}</td>
+        <td>${$id.val()}</td>
+        <td>${$title.val()}</td>
+        <td>$${$salary.val()}</td>
+        </tr>`);
+    
+    //
+    appendSalary();
+
+    //Reset the values in the inputs
+    $first.val('');
+    $last.val('');
+    $id.val('');
+    $title.val('');
+    $salary.val('');
+    
+    console.log(cost);
+}//End submitButtonClick
+
+function appendSalary() {
+    let $salary = $('#salaryInput');
+    cost += parseInt($salary.val())/12;
+    $('#monthlyCost').text(`Total Monthly: $${cost}`);
 }
