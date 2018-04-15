c670d542a1343aa48c558d8c7b71261a3307b077
Greg Orvik <gvorvik@gmail.com>
2018-04-14 20:38:22 -0500

    Add and delete employees to an array as they're created and deleted

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ console.log('ScriptJS Sourced');
 //Set Monthly Cost
 let cost = 0;
 
+//Setup for array of each employee added to DOM
+let employees = [];
+class Employee {
+    constructor(first, last, id, title, salary) {
+        this.firstName = first;
+        this.lastName = last;
+        this.id = id;
+        this.title = title;
+        this.salary = salary;
+    }
+}
+
 $(document).ready(onReady);
+
+
+
+//Functions To Be Hoisted
 
 function onReady() {
     console.log('JQuery Sourced');
@@ -36,6 +52,10 @@ function submitButtonClick() {
     //Update the monthly cost
     appendSalary();
 
+    //Create employee and add to array
+    let employee = addEmployee();
+    employees.push(employee);
+
     //Reset the values in the inputs
     $first.val('');
     $last.val('');
@@ -57,4 +77,12 @@ function appendSalary() {
 //Function to delete last employee
 function deleteEmployee() {
     $('tbody').children().last().remove();
-}
+    employees.pop();
+}//end deleteEmployee
+
+//Function to create new Employee object
+function addEmployee() {
+    let employee = new Employee($('#firstNameInput').val(), $('#lastNameInput').val(), 
+        $('#idInput').val(), $('#titleInput').val(), $('#salaryInput').val());
+    return employee;
+}//end addEmployeeToArray
